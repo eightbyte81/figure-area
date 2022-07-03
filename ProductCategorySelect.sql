f1c6a9db1b06cf3027801f9a419596875442a3bc
Daniil Lyudvig <thricht3r@yandex.ru>
2022-07-03 12:35:01 +0300

    Created SQL query

---
+++ ProductCategorySelect.sql
@@ -0,0 +1,6 @@
+﻿SELECT p.product_name, c.category_name
+FROM product p
+         LEFT JOIN product_category pc
+                   ON pc.product_id = p.product_id
+         LEFT JOIN category c
+                   ON pc.category_id = c.category_id;
